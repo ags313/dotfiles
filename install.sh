42a0cfcaa8c2458e00a3421ce7725554cb689b8a
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2014-01-29 13:29:31 +0000

    defined downloads dir

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 pushd .
 
+cd ~
+mkdir -p downloads/chrome
+mkdir -p downloads/dev
+mkdir -p vcs/misc
+
+export DOWNLOADS="~/downloads"
+
 source install/install-functions.sh
 . install/install-bash.sh
 . install/install-git.sh
